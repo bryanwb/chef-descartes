a73a8f393cf2b3b753acef60fc9f02ce61b6ad5f
Bryan Berry <bryan.berry@gmail.com>
2012-08-22 14:21:40 +0200

    fix postgres config

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default['descartes']['user'] = "descartes"
--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -4,21 +4,30 @@ include_recipe "yumrepo::postgresql9"
    package pkg
 end
 
+ruby_block "initialize database" do
+  block do
+    require 'fileutils'
+    FileUtils.rm_rf "/var/lib/pgsql/9.0/data"
+    system('/etc/init.d/postgresql-9.0 initdb')
+  end
+  not_if {::File.exists? "/var/lib/pgsql/9.0/data" }
+end
+
+
 cookbook_file "/var/lib/pgsql/9.0/data/pg_hba.conf" do
   source "pg_hba.conf"
   owner "postgres"
 end
 
-ruby_block "create database" do
+ruby_block "start database" do
   block do
-    system('/etc/init.d/postgresql-9.0 initdb')
     system('/etc/init.d/postgresql-9.0 start')
   end
 end
 
 bash "create role" do
   code <<-EOH
-  psql -d postgres -c "CREATE ROLE \"fedoraAdmin\" LOGIN PASSWORD 'fedoraAdmin';"
+  psql -d postgres -c "CREATE ROLE \"#{node['descartes']['user']}\" LOGIN PASSWORD '#{node['descartes']['user']}';"
   EOH
   user "postgres"
   ignore_failure true
@@ -26,7 +35,7 @@ end
 
 bash "create database" do
   code <<-EOH
-  psql -d postgres -c "CREATE DATABASE \"fedora\" WITH ENCODING='UTF8' OWNER=\"fedoraAdmin\";"
+  psql -d postgres -c "CREATE DATABASE \"#{node['descartes']['user']}\" WITH ENCODING='UTF8' OWNER=\"#{node['descartes']['user']}\";"
   EOH
   user "postgres"
   ignore_failure true
